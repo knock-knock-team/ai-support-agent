986d6392a951e17f877750d280a9d1244061ba48
jesusdangerous <aleksandrzonov641@gmail.com>
2026-02-25 12:07:56 +0500

    init: init project

---
+++ backend/src/main/java/backend/AiSupportAgentApplication.java
@@ -0,0 +1,13 @@
+package backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSupportAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSupportAgentApplication.class, args);
+	}
+
+}
