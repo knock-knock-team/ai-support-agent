98450e7aa4c76db3d2d246609b93003aed5804ab
jesusdangerous <aleksandrzonov641@gmail.com>
2026-02-26 18:57:18 +0500

    feat: add request category enum

---
+++ backend/src/main/java/com/mailserver/mailprocessor/model/enums/RequestCategory.java
@@ -0,0 +1,11 @@
+package com.mailserver.mailprocessor.model.enums;
+
+public enum RequestCategory {
+    TECHNICAL_SUPPORT,
+    WARRANTY,
+    CONSULTATION,
+    REPAIR,
+    INSTALLATION,
+    CONFIGURATION,
+    OTHER
+}
